b767a73774dbc42b824b620f9a2024da8a6a8abb
nalvarez <nlplalvarez@gmail.com>
2026-03-25 11:01:33 +0800

    Renamed backend structure

---
+++ backend/src/test/java/com/sovereign_ledger/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sovereign_ledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
